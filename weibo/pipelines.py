df5d246d55cc57ef76dae8ad95b03f87592209f7
wlsnx <wanlisnx@gmail.com>
2015-04-08 16:54:12 +0800

    过滤过大的图片

--- weibo/pipelines.py
+++ weibo/pipelines.py
@@ -6,10 +6,18 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-from scrapy.contrib.pipeline.images import ImagesPipeline
+from scrapy.contrib.pipeline.images import ImagesPipeline, ImageException
+from io import BytesIO
+from PIL import Image
 
 
 class WeiboImagePipeline(ImagesPipeline):
+
+    @classmethod
+    def from_settings(cls, settings):
+        cls.MAX_WIDTH = settings.getint("IMAGES_MAX_WIDTH", 10000)
+        cls.MAX_HEIGHT = settings.getint("IMAGES_MAX_HEIGHT", 10000)
+        return super(WeiboImagePipeline, cls).from_settings(settings)
 
     def process_item(self, item, spider):
         self.uid = item["uid"]
@@ -26,4 +34,13 @@ class WeiboImagePipeline(ImagesPipeline):
         if request.url.endswith("gif"):
             path = path[:-3] + "gif"
         return path.replace("thumb/", "thumb/weibo_{}_".format(self.uid))
+
+    def get_images(self, response, request, info):
+        orig_image = Image.open(BytesIO(response.body))
+        width, height = orig_image.size
+        if width > self.MAX_WIDTH or height > self.MAX_HEIGHT:
+            raise ImageException("Image too big (%dx%d > %dx%d)" %
+                                 (width, height, self.MAX_WIDTH, self.MAX_HEIGHT))
+        else:
+            return super(WeiboImagePipeline, self).get_images(response, request, info)
 
