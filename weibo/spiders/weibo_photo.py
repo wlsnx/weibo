999fe01af7cfba5548baf4e56a5eb66f864c5a2f
wlsnx <wanlisnx@gmail.com>
2015-04-08 15:50:16 +0800

    重新读入配置文件

--- weibo/spiders/weibo_photo.py
+++ weibo/spiders/weibo_photo.py
@@ -43,6 +43,9 @@ class WeiboPhotoSpider(WbSpider):
             #self.CLOSE_ON_IDLE = True
 
     def load_config(self):
+        from weibo import settings
+        reload(settings)
+        self.settings.setmodule(settings)
         self.crawler.signals.connect(self.spider_idle, scrapy.signals.spider_idle)
         self.SCRAPE_INTERVAL = self.settings.getint("SCRAPE_INTERVAL", 60)
         self.CLOSE_ON_IDLE = self.settings.getbool("CLOSE_ON_IDLE", True)
